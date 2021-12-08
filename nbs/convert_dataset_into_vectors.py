b8bc25f9b33515f504933c1a6ddc288f78774beb
Kew Jing Sheng <kewjs1994@gmail.com>
2021-12-08 09:37:57 +0800

    add model files

--- nbs/convert_dataset_into_vectors.py
+++ nbs/convert_dataset_into_vectors.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import pickle
 import pandas as pd
@@ -11,55 +10,71 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
 
-BASE_DIR = pathlib.Path().resolve().parent
-DATASET_DIR = BASE_DIR / "data_local"
-EXPORT_DIR = DATASET_DIR/"exports"
-SPAM_DATASET_PATH = EXPORT_DIR/"spam-dataset.csv"
+USE_PROJECT_ROOT = True
+BASE_DIR = pathlib.Path().resolve()
+if USE_PROJECT_ROOT:
+    BASE_DIR = BASE_DIR.parent
+    
+DATASET_DIR = BASE_DIR / "datasets"
+EXPORT_DIR = DATASET_DIR / "exports"
+DATASET_CSV_PATH = EXPORT_DIR / 'spam-dataset.csv'
+TRAINING_DATA_PATH = EXPORT_DIR / 'spam-training-data.pkl'
+print("BASE_DIR is", BASE_DIR)
 
-METADATA_EXPORT_PATH = EXPORT_DIR/"spam-metadata.pkl"
-TOKENIZER_EXPORT_PATH = EXPORT_DIR/"spam-tokenizer.json"
+RUN_DATASET_PREPARE = False
+if RUN_DATASET_PREPARE:
+    # if active, this will download and prepare the dataset.
+    SOURCE_NB = pathlib.Path('01-Prepare_AI_Spam_Classifier_Dataset.ipynb')
+    if SOURCE_NB.exists():
+        %run './{SOURCE_NB}'
+    else:
+        print("01-Prepare_AI_Spam_Classifier_Dataset.ipynb does not exist.")
+        
+if not DATASET_CSV_PATH.exists():
+    raise Exception(f"You must download or create the spam-dataset.csv \n{DATASET_CSV_PATH} not found.")
+
+df = pd.read_csv(str(DATASET_CSV_PATH))
+
+texts = df['text'].tolist()
+labels = df['label'].tolist()
+
+labels_legend = {'ham': 0, 'spam': 1}
+labels_legend_inverted = {f"{v}":k for k,v in labels_legend.items()}
+labels_as_int =  [labels_legend[str(x)] for x in labels]
+
+random_idx = random.randint(0, len(texts))
+print('Random Index', random_idx)
+
+assert texts[random_idx] == df.iloc[random_idx].text
+assert labels[random_idx] == df.iloc[random_idx].label
+assert labels_legend_inverted[str(labels_as_int[random_idx])] == labels[random_idx]
 
 MAX_NUM_WORDS = 280
-MAX_SEQ_LENGTH = 300
-
-model_df = pd.read_csv(SPAM_DATASET_PATH)
-labels = model_df["label"].tolist()
-texts = model_df["text"].tolist()
-
-label_legend = {"ham": 0, "spam": 1}
-label_legend_inverted = {f"{v}": k for k, v in label_legend.items()}
-labels_as_int = [label_legend[x] for x in labels]
-label_legend_inverted[str(labels_as_int[120])]
-
-random_idx = random.randint(0, len(labels))
-
-assert texts[random_idx] == model_df.iloc[random_idx]["text"]
-assert labels[random_idx] == model_df.iloc[random_idx]["label"]
-assert label_legend_inverted[str(labels_as_int[random_idx])] == model_df.iloc[random_idx].label
+MAX_SEQUENCE_LENGTH = 300
 
 tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
 word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
 
-x = pad_sequences(sequences, maxlen=MAX_SEQ_LENGTH)
-labels_as_int_array = np.asarray(labels_as_int)
-y = to_categorical(labels_as_int_array)
+assert len(sequences) == len(texts) == len(labels_as_int)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+X = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+y = to_categorical(np.asarray(labels_as_int))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 training_data = {
-    "x_train": x_train,
-    "x_test": x_test,
-    "y_train": y_train,
-    "y_test": y_test,
-    "max_words": MAX_NUM_WORDS, 
-    "max_seq_length": MAX_SEQ_LENGTH,
-    "legend": label_legend,
-    "label_legend_invereted": label_legend_inverted,
+    'X_train': X_train,
+    'X_test': X_test,
+    'y_train': y_train,
+    'y_test': y_test,
+    'max_words': MAX_NUM_WORDS,
+    'max_sequence': MAX_SEQUENCE_LENGTH,
+    'legend': labels_legend,
+    'labels_legend_inverted': labels_legend_inverted,
+    "tokenizer": tokenizer,
 }
 
-tokenizer_json = tokenizer.to_json()
-TOKENIZER_EXPORT_PATH.write_text(tokenizer_json)
-
-with open(METADATA_EXPORT_PATH, "wb") as f:
+with open(TRAINING_DATA_PATH, 'wb') as f:
     pickle.dump(training_data, f)
